cf35ec9fa5eb011d12ad24814400a08bf02f35ba
Jinhoo Bong <jinhoobong@gmail.com>
2022-03-07 14:38:23 -0800

    Created Links component This component returns all links currently in database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import URL_Form from "./components/URL_Form";
+import Links from "./components/Links";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Hero />
       <URL_Form />
+      <Links />
     </div>
   );
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,1 +1,25 @@
-function Links() {}
+import { useQuery, gql } from "@apollo/client";
+
+function Links() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      allLinks {
+        url
+        slug
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.allLinks.map(({ url, slug }) => (
+    <div>
+      <p>
+        {url} : {slug}
+      </p>
+    </div>
+  ));
+}
+
+module.exports = Links;
