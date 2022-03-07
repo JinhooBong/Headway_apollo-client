1565a8185ae007e1876b477e40b73b656de7bb19
Jinhoo Bong <jinhoobong@gmail.com>
2022-03-07 13:49:45 -0800

    Added tailwind as dependency

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+module.exports = App;
--- src/index.js
+++ src/index.js
@@ -1,47 +1,13 @@
 // start
 import React from "react";
 import { render } from "react-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import App from "./App";
 
 const client = new ApolloClient({
   uri: "https://yi84o5.sse.codesandbox.io/",
   cache: new InMemoryCache()
 });
-
-function Links() {
-  const { loading, error, data } = useQuery(gql`
-    {
-      link(id: 1) {
-        url
-        slug
-      }
-    }
-  `);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return (
-    <p>
-      {data.link.url}:{data.link.slug}
-    </p>
-  );
-}
-
-function App() {
-  return (
-    <div>
-      <h2>Test</h2>
-      <Links />
-    </div>
-  );
-}
 
 render(
   <ApolloProvider client={client}>
